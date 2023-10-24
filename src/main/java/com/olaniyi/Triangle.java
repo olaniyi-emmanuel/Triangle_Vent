9530c87efd10cb2557d65f03b8f701728667a16d
olaniyi-emmanuel <emmanuel4sucess@outlook.com>
2023-10-24 02:06:14 +0100

    Autowiring concept in Spiring

--- src/main/java/com/olaniyi/Triangle.java
+++ src/main/java/com/olaniyi/Triangle.java
@@ -4,19 +4,37 @@ import java.util.List;
 
 public class Triangle {
 
-    private List<Point> points;
+   private Point point1;
+   private  Point point2;
+   private Point point3;
 
-    public List<Point> getPoints() {
-        return points;
+    public Point getPoint1() {
+        return point1;
     }
 
-    public void setPoints(List<Point> points) {
-        this.points = points;
+    public void setPoint1(Point point1) {
+        this.point1 = point1;
+    }
+
+    public Point getPoint2() {
+        return point2;
+    }
+
+    public void setPoint2(Point point2) {
+        this.point2 = point2;
+    }
+
+    public Point getPoint3() {
+        return point3;
+    }
+
+    public void setPoint3(Point point3) {
+        this.point3 = point3;
     }
 
     public  void draw() {
-        for (Point point : points) {
-            System.out.println("Point A = " + point.getX() + ", " + point.getY());
-        }
+        System.out.println("Point 1 = " + point1.getX() + ", " + point1.getY());
+        System.out.println("Point 2 = " + point2.getX() + ", " + point2.getY());
+        System.out.println("Point 3 = " + point3.getX() + ", " + point3.getY());
     }
 }
